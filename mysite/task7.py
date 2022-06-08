0fac6c436f5eca45b9f502158e5952af1be09781
omar-c <anaezechioma@gmail.com>
2022-06-08 22:38:52 +0100

    kodecamp-task2

--- mysite/task7.py
+++ mysite/task7.py
@@ -1,11 +1,13 @@
 from home.models import product
 
+# navigate to python shell on the cmd: python manage.py shell
+# write: from home.models import product 
+# and run the following commands:
+
 #inserting a new record into the product model:
-   #1. python manage.py shell
-   #2. from home.models import product
-   #3. new_product = product(p_name ="hilux", p_price="$500000", p_color="navy-blue", p_brand="ford")
-   #4. new_product.save()
-   #5. product.object.all()
+   #1. new_product = product(p_name ="hilux", p_price="$500000", p_color="navy-blue", p_brand="ford")
+   #2. new_product.save()
+   #3. product.object.all()
 
 #get all records in the product table:
    #1. all_products = product.objects.all() 
@@ -15,7 +17,9 @@ from home.models import product
    #1. post.objects.filter(p_name = "hilux")
 
 #get single record from product:
-   #1. one_product = product.objects.get(pk = 1)   
+#2 methods:
+   #by using a key or id: one_product = product.objects.get(pk = 1)
+   #by using field name: one_product = product.objects.get(p_name = "hilux")  
 
 #to change a product price
    # x = product.objects.get(p_price = "$500000")
